fd6c8b5791204ff053a27faa65a7155a59aadbeb
DarinaStaevska <98123739+DarinaStaevska@users.noreply.github.com>
2022-05-23 11:13:20 +0300

    New

---
+++ RestSharpDemo/RestSharpDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+
+var client = new RestClient("htpps://api.github.com");
+
+string url = "/repos/DarinaStaevska/postman/issues";
+var request = new RestRequest(url);
+request.AddBody(new { title = "New Issue from RestSharp" });
+
+var response = await client.ExecuteAsync(request, Method.Post);
+
+Console.WriteLine("STATUS CODE " + response.StatusCode);
+Console.WriteLine("STATUS CODE " + response.Content);
+
+
